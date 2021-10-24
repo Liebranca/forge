5373ff678c61f9baf6e884b35852dc029bf1bb15
Liebranca <dantepaola25@gmail.com>
2021-10-24 03:11:47 -0300

    found working winapi hello world

--- src/hello.asm
+++ src/hello.asm
@@ -1,12 +1,46 @@
-extern puts
+; taken from https://board.flatassembler.net/topic.php?t=21489
+; uses winapi rather than a straight up C call :>
 
-SECTION .DATA
-  hello:     db 'Hello world!',10,0
+format pe64 console
+entry start
 
-SECTION .TEXT
-  GLOBAL main
+STD_OUTPUT_HANDLE       = -11
 
-main:
-  mov   rcx,hello
-  call  puts
-  ret
+section '.text' code readable executable
+
+start:
+        sub     rsp,8*7
+        mov     rcx,STD_OUTPUT_HANDLE
+        call    [GetStdHandle]
+        mov     rcx,rax
+        lea     rdx,[message]
+        mov     r8d,message_length
+        lea     r9,[rsp+4*8]
+        mov     qword[rsp+4*8],0
+        call    [WriteFile]
+        mov     ecx,eax
+        call    [ExitProcess]
+
+section '.data' data readable writeable
+
+message         db 'Hello World!',0x0A,0x00
+message_length  = $ - message
+
+section '.idata' import data readable writeable
+
+        dd      0,0,0,RVA kernel_name,RVA kernel_table
+        dd      0,0,0,0,0
+
+kernel_table:
+        ExitProcess     dq RVA _ExitProcess
+        GetStdHandle    dq RVA _GetStdHandle
+        WriteFile       dq RVA _WriteFile
+                        dq 0
+
+kernel_name     db 'KERNEL32.DLL',0
+user_name       db 'USER32.DLL',0
+
+_ExitProcess    db 0,0,'ExitProcess',0
+_GetStdHandle   db 0,0,'GetStdHandle',0
+_WriteFile      db 0,0,'WriteFile',0    
+    
