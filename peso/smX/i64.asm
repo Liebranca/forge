6278a6bd3643b506607830fc9ca91c3299ab56f6
Liebranca <dantepaola25@gmail.com>
2023-12-27 10:10:22 -0300

    smX::i64 req specific register

--- peso/smX/i64.asm
+++ peso/smX/i64.asm
@@ -13,8 +13,7 @@
 ; deps
 
 library ARPATH '/forge/'
-  use '.inc' peso::swan
-  use '.asm' peso::smX::common
+  use '.asm' peso::smX::op
 
 library.import
 
@@ -23,7 +22,7 @@ library.import
 
   TITLE     peso.smX.i64
 
-  VERSION   v0.00.5b
+  VERSION   v0.00.6b
   AUTHOR    'IBN-3DILA'
 
 ; ---   *   ---   *   ---
@@ -131,13 +130,10 @@ macro smX.i64.close_scope {
 ;
 ; TODO:
 ;
-; * ask for specific register,
-;   done via List.pluck
-;
 ; * save and load from stack
 ;   when we run out of registers
 
-macro smX.i64.get_mem dst,args {
+macro smX.i64.get_mem dst,args,which= {
 
   match scp , i64.cscope \{
 
@@ -151,17 +147,18 @@ macro smX.i64.get_mem dst,args {
     ; ^got scratch
     match any,have \\{
 
-      local name
+      ; get register
+      local rX
+      smX.i64.pick_mem scp,alloc,rX,which
 
-      scp\#.avail.shift name
-      smX.i64.new_mem dst,args+name
-
+      ; ^mark in use
+      smX.i64.new_mem dst,args+rX
       scp\#.mems.push dst
 
     \\}
 
 
-    ; ^TODO: none avail, move to stack
+    ; ^none avail, move to stack
     match , have \\{
       out@err "NYI stack mem @ i64.scope"
 
@@ -181,21 +178,9 @@ macro smX.i64.free_mem which= {
 
   match scp , i64.cscope \{
 
+    ; get register
     local rX
-    rX equ
-
-
-    ; no name passed
-    match , which \\{
-      scp\#.mems.pop rX
-
-    \\}
-
-    ; ^name passed, hold on...
-    match any , which \\{
-      scp\#.mems.pluck rX,any
-
-    \\}
+    smX.i64.pick_mem scp,free,rX,which
 
 
     ; ^give back to pool
@@ -210,464 +195,43 @@ macro smX.i64.free_mem which= {
 }
 
 ; ---   *   ---   *   ---
-; operation struc
+; picks register from avail
 
-swan.new i64.op
-swan.attr name,xor
-swan.attr elems,2
+macro smX.i64.pick_mem scp,mode,rX,which= {
 
-swan.attr elem_A,null
-swan.attr elem_B,null
-swan.attr elem_C,null
+  rX equ
 
-swan.attr elem_A_deref,null
-swan.attr elem_B_deref,null
-swan.attr elem_C_deref,null
+  ; take from scope
+  match =alloc , mode \{
 
-swan.attr elem_A_tmp,null
-swan.attr elem_B_tmp,null
-swan.attr elem_C_tmp,null
+    ; no name passed
+    match , which \\{
+      scp#.avail.shift rX
 
-swan.end
+    \\}
 
-; ---   *   ---   *   ---
-; ^ROM
-
-  define i64.op.xor_elems  2
-  define i64.op.or_elems   2
-
-  define i64.op.mov_elems  2
-  define i64.op.cmp_elems  2
-  define i64.op.test_elems 2
-
-; ---   *   ---   *   ---
-; ^cstruc
-
-macro smX.i64.op.new dst,op,item& {
-
-  ; generate iced
-  local uid
-  uid.new uid,smX.i64.op,global
-
-  ; ^unroll and make ice
-  match id , uid \{
-
-    i64.op.new id,\
-      name  => op,\
-      elems => i64.op.#op#_elems
-
-    dst equ id
-
-    ; nit operands if passed
-    match elems , item \{
-      smX.i64.op.set_elems id,elems
-
-    \}
-
-  \}
-
-}
-
-; ---   *   ---   *   ---
-; ^assign operands
-
-macro smX.i64.op.set_elems %O,[item] {
-
-  ; nit operand id
-  common
-    local elem
-    elem equ A
-
-
-  ; ^set
-  forward
-
-    ; throw on items > 3
-    match =END , elem \{
-      out@err "Overargs for i64.op ",`op
-
-    \}
-
-    match any,elem \{
-      %O#.elem_\#any\#.set item
-
-    \}
-
-    ; ^go next
-    match =C , elem \{
-      elem equ END
-
-    \}
-
-    match =B , elem \{
-      elem equ C
-
-    \}
-
-    match =A , elem \{
-      elem equ B
-
-    \}
-
-}
-
-; ---   *   ---   *   ---
-; dstruc
-
-macro i64.op.del %O {
-  i64.op.bat_free_tmp %O,A,B,C
-  %O#.del
-
-}
-
-; ---   *   ---   *   ---
-; ^cleanup tmps
-
-macro i64.op.free_tmp %O,UDN {
-
-  ; get tmp used
-  local status
-  status equ 0
-
-  match =null,%O#.elem_#UDN#_tmp \{
-    status equ 1
-
-  \}
-
-  ; ^yep, give back
-  match =0 any , status %O#.elem_#UDN#_tmp \{
-    smX.i64.free_mem any
-    %O#.elem_#UDN#_tmp equ null
-
-  \}
-
-}
-
-; ---   *   ---   *   ---
-; ^bat
-
-macro i64.op.bat_free_tmp %O,[UDN] {
-  forward i64.op.free_tmp %O,UDN
-
-}
-
-; ---   *   ---   *   ---
-; unpacks all operands
-; to make call
-
-macro i64.op.funroll %O,fn {
-
-  ; elems
-  local UA
-  local UB
-  local UC
-
-  ; ^unpack
-  match A B C , \
-    %O#.elem_A \
-    %O#.elem_B \
-    %O#.elem_C \
-  \{
-
-    UA equ A
-    UB equ B
-    UC equ C
-
-  \}
-
-
-  ; no operands
-  match =0 , %O#.elems \{i64.op.#fn\}
-
-  ; ^single operand
-  match =1 , %O#.elems \{
-    match A , UA \\{i64.op.#fn %O,A\\}
-
-  \}
-
-  ; ^two operands
-  match =2 , %O#.elems \{
-    match A B , UA UB \\{
-      i64.op.#fn %O,A,B
+    ; ^name passed, hold on...
+    match any , which \\{
+      scp#.avail.pluck rX,any
 
     \\}
 
   \}
 
-  ; ^three operands
-  match =3 , %O#.elems \{
-    match A B C , UA UB UC \\{
-      i64.op.#fn %O,A,B,C
+  ; ^give back!
+  match =free , mode \{
+
+    ; no name passed
+    match , which \\{
+      scp#.mems.pop rX
 
     \\}
 
-  \}
-
-}
-
-; ---   *   ---   *   ---
-; ^unpacks *passed* operands
-; to make call
-
-macro i64.op.unroll %O,fn,[item] {
-
-  common
-    local list
-    list equ
-
-  forward match elem , %O#.elem_#item \{
-    commacat list,elem
-
-  \}
-
-  common match any,list \{
-    i64.op.#fn %O,any
-
-  \}
-
-}
-
-; ---   *   ---   *   ---
-; makes variation in op call
-; where fn@[UDN] args is fn args,[UDN]
-
-macro i64.op.icecall base,[udn] {
-
-  ; cat [UDN-0..UDN-X]
-  common
-    local udncat
-    udncat equ +
-
-  forward match any , udncat \{
-    udncat equ any\#udn
-
-  \}
-
-  ; ^make base@UDN wraps
-  ; with [UDN-0..UDN-X] as implicit args
-  common match =+ ext , udncat \{
-
-    macro i64.op.#base#@\#ext %O,args& \\{
-
-      local ok
-      ok equ 0
-
-      ; need args!
-      match ext2 list, ext args \\\{
-        i64.op.#base %O,list,udn
-        ok equ 1
-
-      \\\}
-
-      ; ^no args ;>
-      match =0 , ok \\\{
-        i64.op.#base %O,udn
-
-      \\\}
+    ; ^name passed, hold on...
+    match any , which \\{
+      scp#.mems.pluck rX,any
 
     \\}
-
-  \}
-
-}
-
-; ---   *   ---   *   ---
-; conditional dereference
-; of source operand
-
-macro i64.op.need_deref? %O,UD0,UD1,UDN {
-
-  match =m , UD1#.mode \{
-
-    ; mem to mem, do tmpchk
-    match =m , UD0#.mode \\{
-      i64.op.need_repl?@#UDN %O,UD1
-
-    \\}
-
-    ; ^no tmp needed ;>
-    match =r , UD0#.mode \\{
-
-      cline %O#.name \
-        UD0#.loc,UD1#.size [UD1#.xloc+UD1#.off]
-
-    \\}
-
-  \}
-
-  ; ^rY to rX?
-  match =r , UD1#.mode \{
-    i64.op.deref_dst? %O,UD0,UD1
-
-  \}
-
-}
-
-; ---   *   ---   *   ---
-; ^iceof
-
-i64.op.icecall need_deref?,B
-
-; ---   *   ---   *   ---
-; overwrite or use tmp?
-
-macro i64.op.need_repl? %O,UD0,UDN {
-
-  ; replace self
-  match =1 , UD0#.repl \{
-
-    cline mov \
-      UD0#.loc,UD0#.size [UD0#.xloc+UD0#.off]
-
-    %O#.elem_#UDN#_deref.set UD0
-
-  \}
-
-
-  ; ^get new reg
-  local ok
-  ok equ 0
-
-  match =0 =null ,\
-    UD0#.repl \
-    %O#.elem_#UDN#_tmp \{
-
-    local uid
-    smX.i64.get_mem uid,UD0#.size r
-
-    ; ^use as tmpdst
-    match id , uid \\{
-
-      cline mov \
-        id\\#.loc,\
-        UD0#.size [UD0#.xloc+UD0#.off]
-
-      %O#.elem_#UDN#_deref.set id
-      %O#.elem_#UDN#_tmp.set   id
-
-    \\}
-
-    ok equ 1
-
-  \}
-
-  ; ^already have tmp ;>
-  match =0 =0 id ,\
-    UD0#.repl ok \
-    %O#.elem_#UDN#_tmp \{
-
-    %O#.elem_#UDN#_deref.set id
-    %O#.elem_#UDN#_tmp.set   id
-
-    cline mov \
-      id\\#.loc,\
-      UD0#.size [UD0#.xloc+UD0#.off]
-
-  \}
-
-}
-
-; ---   *   ---   *   ---
-; ^iceof
-
-i64.op.icecall need_repl?,A
-i64.op.icecall need_repl?,B
-
-; ---   *   ---   *   ---
-; overwrite dst?
-
-macro i64.op.deref_dst? %O,UD0,UD1 {
-
-  ; [dst] <= src
-  match =m , UD0#.mode \{
-
-    cline %O#.name \
-      UD0#.size [UD0#.xloc+UD0#.off],\
-      UD1#.loc
-
-  \}
-
-  ; ^dst <= src
-  match =r , UD0#.mode \{
-    cline %O#.name UD0#.loc,UD1#.loc
-
-  \}
-
-}
-
-; ---   *   ---   *   ---
-; [?dst] <= [?src] proto
-
-macro i64.op.cderef %O,UDN0,UDN1 {
-
-  local status
-  status equ 0
-
-  ; deref or solve?
-  i64.op.funroll %O,need_deref?@#UDN1
-
-  match UD1 , %O#.elem_#UDN1#_deref \{
-
-    ; ^solved ;>
-    match =null , UD1 \\{
-      status equ 1
-
-    \\}
-
-    ; ^deref, second step needed
-    match =0 UD0 , status %O#.elem_#UDN0 \\{
-      i64.op.deref_dst? %O,UD0,UD1
-
-    \\}
-
-  \}
-
-}
-
-; ---   *   ---   *   ---
-; ^iceof
-
-i64.op.icecall cderef,A,B
-
-; ---   *   ---   *   ---
-; generic: ins [?dst] <= [?src]
-
-macro i64.op.B2A ins {
-
-  ; make op ice
-  macro i64.#ins dst,UD0,UD1 \{
-    smX.i64.op.new uid,ins,UD0,UD1
-    dst equ uid
-
-    ; ^lis methods
-    match %O , uid \\{
-
-      macro %O\\#.run \\\{
-        i64.op.cderef@AB %O
-
-      \\\}
-
-    \\}
-
-
-  \}
-
-}
-
-; ---   *   ---   *   ---
-; ^iceof
-
-i64.op.B2A mov
-i64.op.B2A xor
-i64.op.B2A or
-
-; ---   *   ---   *   ---
-; ^run F for all items
-
-macro i64.op.batrun fn,[item] {
-
-  forward match %O any , item fn \{
-    %O\#.\#any
 
   \}
 
@@ -687,6 +251,7 @@ swan.attr repl,0
 swan.attr mode,r
 
 swan.end
+
 ; ---   *   ---   *   ---
 ; ^cstruc
 
